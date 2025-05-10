09e24b8c01eedfa99a6132070e03cc0746d1cd58
cgarrigoza97 <31805380+cgarrigoza97@users.noreply.github.com>
2025-05-10 05:25:37 -0300

    use alt line encoding

--- src/chunk.rs
+++ src/chunk.rs
@@ -9,10 +9,21 @@ pub enum OpCode {
     OpReturn
 }
 
+pub struct LineEncoding {
+    pub line_number: i32,
+    pub repetition_count: i32
+}
+
+impl LineEncoding {
+    pub fn new(line: i32) -> Self {
+        Self { line_number: line, repetition_count: 1 }
+    }
+}
+
 pub struct Chunk {
     pub code: Vec<u8>,
     pub constants: ValueArray,
-    pub lines: Vec<i32>
+    pub lines: Vec<LineEncoding>
 }
 
 impl Chunk {
@@ -26,7 +37,29 @@ impl Chunk {
 
     pub fn write_chunk(&mut self, byte: u8, line: i32) {
         self.code.push(byte);
-        self.lines.push(line);
+        
+        if let Some(last) = self.lines.last_mut() {
+            if last.line_number == line {
+                last.repetition_count += 1;
+                return;
+            }
+        }
+
+        self.lines.push(LineEncoding::new(line));
+    }
+    
+    pub fn get_line(&self, code_index: usize) -> i32 {
+        let mut total_code_count: usize = 0;
+    
+        for i in 0..self.lines.len() {
+            total_code_count += self.lines[i].repetition_count as usize;
+
+            if code_index < total_code_count {
+                return self.lines[i].line_number;
+            }
+        }
+
+        return 0;
     }
 
     pub fn add_constant(&mut self, value: Value) -> u8 {
--- src/debug.rs
+++ src/debug.rs
@@ -12,11 +12,9 @@ pub fn disassemble_chunk(chunk: &Chunk, name: &str) {
 
 pub fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
     print!("{:04} ", offset);
-    if offset > 0 && chunk.lines[offset] == chunk.lines[offset - 1] {
-        print!("   | ");
-    } else {
-        print!("{:04} ", chunk.lines[offset]);
-    }
+    let line_number = chunk.get_line(offset);
+    print!("{:04} ", line_number);
+
 
     let instruction = chunk.code[offset];
     let instruction_op_code = num::FromPrimitive::from_u8(instruction);
